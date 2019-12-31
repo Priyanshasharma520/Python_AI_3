81f26fa8cc60802ffb835250aa35fd5e748a256b
Priyansha Sharma <55023456+Priyanshasharma520@users.noreply.github.com>
2019-12-31 12:46:00 +0530

    pyqrcode

---
+++ pyqrcode_1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pyqrcode
+
+
+# In[7]:
+
+
+mydata =pyqrcode.create("https://github.com/redashu/pywinter2019/blob/master/ashu_sender.py")
+mydata.svg("github_1.svg",scale=15)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
